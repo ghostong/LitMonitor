8da4d08b92499329269ffbf794bdf057d6161f0d
李通 <ghostong@126.com>
2018-05-29 17:47:13 +0800

    添加WebUi

--- bin/CmdUi.py
+++ bin/CmdUi.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys, os, json
+import sys, os, json, time
 sys.path.append( os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) ) + "/src" )
 import Main
 
 ServerPort = Main.CONFIG.get('DATA_SERVER','DATA_PORT')
 ServerHost = Main.CONFIG.get('DATA_SERVER','DATA_HOST')
+ColumeOrder= Main.CONFIG.get('CMDUI_CONFIG','COLUMN_ORDER').split(',')
 
 def GetMoniData () :
     Data = {}
@@ -18,21 +19,63 @@ def GetMoniData () :
 
 def DataForMat ( Data ) :
     NewData = {}
-    Title = {'HostName':'Name','HostIp':'IP','Cpu':'Cpu','Mem':'Mem','Swap':'Swap','Task':'Task','Disk':'Disk'}
+    Title = {'Name':'Name','Ip':'Ip','Cpu':'Cpu','Mem':'Mem','Swap':'Swap','Task':'Task','Disk':'Disk','Time':'Time'}
     NewData['Title'] = Title
     for i in Data :
         d = Data[i]
         Disk = ','.join( d['AgentData']['DiskList'].values() )
+        Time = time.strftime( "%H:%M:%S", time.localtime( d['AgentData']['LastTime'] ) )
         Tmp = { \
-                'HostName':d['HostName'],'HostIp':d['HostIp'],'Cpu':d['AgentData']['Cpu']+'%',\
+                'Name':d['HostName'],'Ip':d['HostIp'],'Cpu':d['AgentData']['Cpu']+'%',\
                 'Mem':d['AgentData']['MemUse']+'M/'+d['AgentData']['MemTotal']+'M',\
                 'Swap':d['AgentData']['SwapUse']+'M/'+d['AgentData']['SwapTotal']+'M',\
-                'Task':d['AgentData']['Task'],'Disk':Disk\
+                'Task':d['AgentData']['Task'],'Disk':Disk,'Time':Time\
                 }
         NewData[i] = Tmp
     return NewData
 
+def ColumnCalc ( Data ) :
+    NewData = {}
+    for i in Data.values() :
+        for j in i :
+            NewData[j] = len(i[j]) if ( len(i[j]) > NewData.get(j,0) ) else NewData.get(j,0)
+    return NewData
+
+def BuildTopLine ( Data ) :
+    RowLen = ( len( Data ) - 1 ) * 3 + 4 + sum ( Data.values() ) - 2
+    Line = '+' + ('-'*RowLen) + '+'
+    return Line
+
+def BuildDataLine ( Data ) :
+    Tmp = []
+    EndStr = '| '
+    for i in ColumeOrder :
+        TitleLen = len(Data[i])
+        Half = int(( ColuCalc[i] - TitleLen ) / 2)
+        Tmp.append(' '*Half + Data[i] + ' '*( ColuCalc[i]- TitleLen - Half))
+    EndStr += ' | '.join( Tmp )
+    EndStr += ' |'
+    return EndStr
+
+def PrintScreen ( ColuCalc, ForMatRes ) :
+    os.system('clear')
+    Line = BuildTopLine(ColuCalc)
+    OutStr = Line + "\n"
+    OutStr += BuildDataLine ( ForMatRes['Title'] )  + "\n"
+    OutStr += Line + "\n"
+    ForMatRes.pop('Title')
+    for i in ForMatRes :
+        OutStr += BuildDataLine ( ForMatRes[i] )  + "\n"
+    OutStr += Line  + "\n"
+    print OutStr
+    #sys.stdout.write( OutStr + "\r" )
+    #sys.stdout.clear()
+
 
 if __name__=='__main__' :
-    MoniData = GetMoniData()
-    DataForMat(MoniData)
+    while 1 :
+        MoniData  = GetMoniData()
+        ForMatRes = DataForMat( MoniData )
+        ColuCalc  = ColumnCalc( ForMatRes )
+        PrintScreen ( ColuCalc , ForMatRes )
+        time.sleep (2);
